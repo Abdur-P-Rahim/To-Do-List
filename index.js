400ac23c1d364cb2bc83f49e2fd7302f42afc15f
Abdur-p-Rahim <bspirahim@gmail.com>
2022-07-23 16:04:52 +0600

    delete button added

--- index.js
+++ index.js
@@ -12,10 +12,20 @@ document.getElementById('add-btn').addEventListener('click', function (){
           tableContainer.innerHTML = ` <th>${count}</th>
                                        <td>${inputValue}</td>
                                        <td>
-                                       <button class = 'btn btn-danger '>Delete</button>
-                                       <button class = 'btn btn-success'>Done</button>
+                                       <button class = 'btn btn-danger delete-btn'>Delete</button>
+                                       <button class = 'btn btn-success done-btn'>Done</button>
                                        </td>`;
           mainContainer.appendChild(tableContainer); 
-          document.getElementById('input-value').value = '';                            
+          document.getElementById('input-value').value = ''; 
+          
+          const deleteBtn = document.getElementsByClassName('delete-btn');
+          const doneBtn = document.getElementsByClassName('done-btn');
+          for (const button of deleteBtn){
+               button.addEventListener('click', function(e){
+                    e.target.parentNode.parentNode.style.display = 'none';
+               });
+          }
+
+       
      }
 });
